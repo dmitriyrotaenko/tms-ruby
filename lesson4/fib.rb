77d781dfe1eceefc63e43375c0a4a841231f839d
drotaenko <drotaenko@xbsoftware.by>
2023-01-03 19:14:22 +0300

    feat: Add fib numbers

---
+++ lesson4/fib.rb
@@ -0,0 +1,16 @@
+# Задача:
+# Заполнить массив числами фибоначчи до 100
+
+fib_nums = [0, 1]
+cur_idx = 1
+limit = 100
+
+while fib_nums[cur_idx] < limit
+  cur_num = fib_nums[cur_idx]
+  prev_num = fib_nums[cur_idx - 1]
+  next_num = cur_num + prev_num
+  next_num > limit ? break : fib_nums << next_num
+
+  cur_idx += 1
+end
+
