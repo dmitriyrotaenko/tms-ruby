49ff3cbd81aeb4c74c681fe1167e697e12736a9a
drotaenko <drotaenko@xbsoftware.by>
2023-01-05 19:37:48 +0300

    fix: remove unnecessary variables

--- lesson4/fib.rb
+++ lesson4/fib.rb
@@ -6,9 +6,7 @@ cur_idx = 1
 limit = 100
 
 while fib_nums[cur_idx] < limit
-  cur_num = fib_nums[cur_idx]
-  prev_num = fib_nums[cur_idx - 1]
-  next_num = cur_num + prev_num
+  next_num = fib_nums[cur_idx] + fib_nums[cur_idx - 1]
   next_num > limit ? break : fib_nums << next_num
 
   cur_idx += 1
