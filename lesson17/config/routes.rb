befc7407001e592de1db86f0544f6f7b38a2f816
drotaenko <drotaenko@xbsoftware.by>
2023-02-26 20:03:56 +0300

    feat: add chucknorris API

--- lesson17/config/routes.rb
+++ lesson17/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/jokes/random', to: 'jokes#random'
+  get '/jokes/categories', to: 'jokes#categories'
+  get '/jokes/search', to: 'jokes#search'
 end
