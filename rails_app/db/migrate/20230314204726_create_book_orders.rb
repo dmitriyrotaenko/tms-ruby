dc35ca9fefd58e061d103db41644dafd21f5ae0e
drotaenko <drotaenko@xbsoftware.by>
2023-03-14 23:48:27 +0300

    feat: generate, describe, and seed models

---
+++ rails_app/db/migrate/20230314204726_create_book_orders.rb
@@ -0,0 +1,9 @@
+class CreateBookOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :book_orders do |t|
+      t.integer :book_id
+      t.integer :order_id
+      t.timestamps
+    end
+  end
+end
