68d15b6643a4fcc4607411e92fab9432a30ee61a
Dmitriy Rotaenko <rotaenko.dmitriy@gmail.com>
2023-04-18 19:53:10 +0300

    feat: Added AvailableCurrencies service

--- currencies/app/controllers/currencies_controller.rb
+++ currencies/app/controllers/currencies_controller.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
-
 class CurrenciesController < ApplicationController
-  API_URL = 'https://www.freeforexapi.com'
-  API_PATH = '/api/live'
-
   def index
-    currencies_request = HttpFetcher.call(API_URL + API_PATH)
-    render json: remove_duplicates(
-      FieldExtractor.call({ obj: currencies_request, field: 'supportedPairs' })
-    )
+    render json: Currency::AvailableCurrencies.call
   end
 
   def convert
@@ -16,14 +9,5 @@ class CurrenciesController < ApplicationController
       amount: params[:amount],
       rate: Currency::Rate.call({ from: params[:from], to: params[:to] })
     })
-  end
-
-  private
-
-  def remove_duplicates(pairs)
-    pairs.map { |pair| [pair[0..2], pair[3..]] }
-         .flat_map { |from, to| [from, to] }
-         .uniq
-         .sort
   end
 end
